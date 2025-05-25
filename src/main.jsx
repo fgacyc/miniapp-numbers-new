c0385f718ffb22ee4ec4937ddb5ce0fa04befcf0
Innis <1575270674@qq.com>
2025-05-25 22:33:26 +0800

    feat : add arco enUS

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import {
     QueryClient,
     QueryClientProvider,
 } from "@tanstack/react-query";
+import {ConfigProvider} from '@arco-design/web-react';
+import enUS from '@arco-design/web-react/es/locale/en-US';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={new QueryClient()}>
-            <App/>
+            <ConfigProvider locale={enUS}>
+                <App/>
+            </ConfigProvider>
         </QueryClientProvider>
     </React.StrictMode>,
 )
